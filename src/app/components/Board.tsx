e038a75f8571d3c7d67ace59272c2fd10f3e71d5
Jeffkimaiyo <jeffkimaiyo@gmail.com>
2025-01-23 17:19:21 +0300

    changes

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useEffect, useState } from "react";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Image from "next/image";
 
 const Board = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Board from "./components/Board";
 export default function Home() {
   return (
